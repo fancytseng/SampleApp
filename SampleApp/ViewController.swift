7f57427bc770794048686f2077f32e3ebe6ec734
fancytseng <55032412+fancytseng@users.noreply.github.com>
2021-01-31 17:38:24 -0800

    Finished Assignment 1 - press button to see name

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblPressMe: UILabel!
+    
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    var playBtnIsVisible : Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func pressMeAction(_ sender: Any) {
+        lblPressMe.text = "Button Pressed"
+        if playBtnIsVisible == true {
+            playBtnIsVisible = false
+            imgView.image = UIImage(named: "Tseng")
+        }
+        else {
+            playBtnIsVisible = true
+            imgView.image = UIImage(named: "Fancy")
+        }
+    }
+    
 }
 
